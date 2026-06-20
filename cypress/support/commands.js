7ef27700cc5ec783b7f7852630ba3d0cca569613
natacharodriguez <natu.r@live.com.ar>
2026-06-20 19:49:11 +0200

    feat: completar reserva exitosa con custom commands, fixtures y fechas dinámicas

--- cypress/support/commands.js
+++ cypress/support/commands.js
@@ -1,25 +1,24 @@
-// ***********************************************
-// This example commands.js shows you how to
-// create various custom commands and overwrite
-// existing commands.
-//
-// For more comprehensive examples of custom
-// commands please read more here:
-// https://on.cypress.io/custom-commands
-// ***********************************************
-//
-//
-// -- This is a parent command --
-// Cypress.Commands.add('login', (email, password) => { ... })
-//
-//
-// -- This is a child command --
-// Cypress.Commands.add('drag', { prevSubject: 'element'}, (subject, options) => { ... })
-//
-//
-// -- This is a dual command --
-// Cypress.Commands.add('dismiss', { prevSubject: 'optional'}, (subject, options) => { ... })
-//
-//
-// -- This will overwrite an existing command --
-// Cypress.Commands.overwrite('visit', (originalFn, url, options) => { ... })
+// Comando para generar una fecha futura
+Cypress.Commands.add('generarFechaFutura', (diasAAdicionar) => {
+    const fecha = new Date();
+    fecha.setDate(fecha.getDate() + diasAAdicionar);
+    
+    const dia = String(fecha.getDate()).padStart(2, '0');
+    const mes = String(fecha.getMonth() + 1).padStart(2, '0');
+    const anio = fecha.getFullYear();
+    
+    return cy.wrap({
+        paraEscribir: `${dia}/${mes}/${anio}`,   // Para el formulario
+        paraValidar: `${anio}-${mes}-${dia}`     // Para el cartel de éxito final
+    });
+});
+
+// Comando para completar el formulario
+Cypress.Commands.add('completarFormularioReserva', (fixtureName) => {
+    cy.fixture(fixtureName).then((usuario) => {
+        cy.get('input[name="firstname"]').should('be.visible').type(usuario.firstname);
+        cy.get('input[name="lastname"]').type(usuario.lastname);
+        cy.get('input[name="email"]').type(usuario.email);
+        cy.get('input[name="phone"]').type(usuario.phone);
+    });
+});
